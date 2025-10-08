6ce7b1c851719a535892091554134fed025bf6c4
Cody Confer <mail@codyconfer.me>
2025-10-08 02:18:03 -0600

    Update dip.sh

--- src/assets/.sauce/droid/dip.sh
+++ src/assets/.sauce/droid/dip.sh
@@ -33,7 +33,7 @@ eval "$(pyenv init -)"
 
 # nvm
 export NVM_DIR=$HOME/.nvm
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh\"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 export PATH=$NVM_DIR:$PATH
 '
