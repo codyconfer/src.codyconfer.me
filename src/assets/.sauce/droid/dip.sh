b9f3ceae71b4e29eadb3b82c57eed9647b864fa7
Cody Confer <mail@codyconfer.me>
2025-10-08 01:49:51 -0600

    Update dip.sh

--- src/assets/.sauce/droid/dip.sh
+++ src/assets/.sauce/droid/dip.sh
@@ -37,169 +37,19 @@ export NVM_DIR=$HOME/.nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 export PATH=$NVM_DIR:$PATH
 '
-echo $profile >> $HOME/.bashrc
-zshrc='
+zshrc='# .oh-my-posh
 export PATH=$PATH:$HOME/.local/bin
-eval "$(oh-my-posh init zsh --config $HOME/.sauce/themes/sauce.ohmyposh.toml)"
+eval "$(oh-my-posh init zsh)"
 '
-sauce_omp='
-#:schema https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/schema.json
-
-version = 3
-
-[transient_prompt]
-template = "{{ now | date \"15:04:05\" }} ❯ "
-foreground = "magenta"
-background = "transparent"
-
-[[blocks]]
-type = "prompt"
-alignment = "left"
-newline = true
-
-[[blocks.segments]]
-template = "{{ if gt .Code 0 }}<#ff0000></>{{ else }}<#23d18b></>{{ end }} "
-type = "status"
-style = "plain"
-
-[blocks.segments.properties]
-always_enabled = true
-cache_duration = "none"
-
-[[blocks.segments]]
-template = " took  {{ .FormattedMs }} "
-foreground = "yellow"
-type = "executiontime"
-style = "plain"
-
-[blocks.segments.properties]
-cache_duration = "none"
-threshold = 10.0
-
-[[blocks]]
-type = "prompt"
-alignment = "left"
-newline = true
-
-[[blocks.segments]]
-template = "{{ if .WSL }}WSL at {{ end }}{{.Icon}} "
-foreground = "cyan"
-type = "os"
-style = "plain"
-
-[blocks.segments.properties]
-cache_duration = "none"
-
-[[blocks.segments]]
-template = " {{ .UserName }}@{{ .HostName }} "
-foreground = "cyan"
-type = "session"
-style = "diamond"
-
-[blocks.segments.properties]
-cache_duration = "none"
-
-[[blocks.segments]]
-template = "{{ .UpstreamIcon }}{{ .HEAD }}{{if .BranchStatus }} {{ .BranchStatus }}{{ end }}{{ if .Working.Changed }}  {{ .Working.String }}{{ end }}{{ if and (.Working.Changed) (.Staging.Changed) }} |{{ end }}{{ if .Staging.Changed }}  {{ .Staging.String }}{{ end }}{{ if gt .StashCount 0 }}  {{ .StashCount }}{{ end }} "
-foreground = "#FFE700"
-type = "git"
-style = "plain"
-foreground_templates = [
-  "{{ if and (gt .Ahead 0) (gt .Behind 0) }}#FFCC80{{ end }}",
-  "{{ if gt .Ahead 0 }}#16c60c{{ end }}",
-  "{{ if gt .Behind 0 }}#f450de{{ end }}",
-]
-
-[blocks.segments.properties]
-cache_duration = "none"
-fetch_status = true
-fetch_upstream_icon = true
-
-[[blocks]]
-type = "prompt"
-alignment = "right"
-
-[[blocks.segments]]
-template = "  {{ .Full }} "
-foreground = "cyan"
-type = "dotnet"
-style = "plain"
-
-[blocks.segments.properties]
-cache_duration = "none"
-
-[[blocks.segments]]
-template = "  {{ .Full }} "
-foreground = "cyan"
-type = "go"
-style = "plain"
-
-[blocks.segments.properties]
-cache_duration = "none"
-
-[[blocks.segments]]
-template = "  ({{ if .Error }}{{ .Error }}{{ else }}{{ if .Venv }}{{ .Venv }} {{ end }}{{ .Major }}.{{ .Minor }}{{ end }}) "
-foreground = "#ffd343"
-type = "python"
-style = "plain"
-
-[blocks.segments.properties]
-cache_duration = "none"
-
-[[blocks.segments]]
-template = "  {{ .Full }} "
-foreground = "#6CA35E"
-type = "node"
-style = "plain"
-
-[blocks.segments.properties]
-cache_duration = "none"
-
-[[blocks.segments]]
-template = "  {{ .Full }} "
-foreground = "#f80000"
-type = "java"
-style = "plain"
-
-[blocks.segments.properties]
-cache_duration = "none"
-
-[[blocks]]
-type = "prompt"
-alignment = "left"
-newline = true
-
-[[blocks.segments]]
-template = "{{ .Path }} "
-foreground = "cyan"
-type = "path"
-style = "plain"
-
-[blocks.segments.properties]
-cache_duration = "none"
-style = "full"
-
-[[blocks.segments]]
-template = "❯ "
-foreground = "green"
-type = "text"
-style = "plain"
-
-[blocks.segments.properties]
-cache_duration = "none"
-'
-theme_dir="$HOME/.sauce/themes"
-sudo apt install -y zsh
-sudo mkdir -p $theme_dir
-echo $sauce_omp > $theme_dir/sauce.ohmyposh.toml
-echo "$zshrc $profile" > .zshrc
-sudo chsh $(whoami) -s /usr/bin/zsh
+echo $profile >> $HOME/.bashrc
+sudo apt install -y zsh unzip neovim git gh
+echo "$profile$zshrc" > .zshrc
 curl -s https://ohmyposh.dev/install.sh | bash -s
+sudo chsh $(whoami) -s /usr/bin/zsh
 sudo apt install -y make build-essential libssl-dev \
 zlib1g-dev libreadline-dev libsqlite3-dev wget curl \
 llvm libncursesw5-dev xz-utils tk-dev libxml2-dev \
-unzip libxmlsec1-dev libffi-dev liblzma-dev neovim \
-git gh
+libxmlsec1-dev libffi-dev liblzma-dev
 sudo wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
 sudo chmod +x ./dotnet-install.sh
 for v in "${dotnet_versions[@]}"; do
